8a4b675f34d432003baa785e4772b33a5f70fcff
Aditya Narwade <adityanarwade111@gmail.com>
2026-01-12 20:33:52 +0530

    Factorial code completed

---
+++ basic/factorial.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class factorial {
+    public static void main(String[] args) {
+        int input = 5;
+        int ans = 1;
+        while(input != 1){
+            ans *= input--;
+        }
+        System.out.println(ans);
+
+    }
+}
